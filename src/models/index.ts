85212476be71e77f71931069725461c8387a98be
abejaoui <ahmed@abejaoui.com>
2022-12-26 16:17:41 +0100

    :art: add variable envs

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,13 +2,13 @@ import * as AWS from "aws-sdk";
 
 let options = {};
 
-console.log("is offline", process.env.IS_OFFLINE)
+const { DYNAMODB_LOCAL_ENDPOINT , DYNAMODB_LOCAL_REGION } = process.env
 
 // connect to local DB if running offline
 if (process.env.IS_OFFLINE) {
   options = {
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
+    region: DYNAMODB_LOCAL_REGION,
+    endpoint: DYNAMODB_LOCAL_ENDPOINT,
   };
 }
 
